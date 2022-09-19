8ce0fc59be0b8750df4d57bc152026ba0258eec9
Marianela <maruuariasg@gmail.com>
2022-09-19 03:45:52 -0300

    sin db

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-})
+// const connection = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE
+// })
 
 
 app.set('view engine', 'hbs');
@@ -35,65 +35,65 @@ app.get('/index.hbs', (req, res) => {
 
 });
 
-app.post('/index.hbs', async (req, res) => {
-    const suscripciones = req.body.suscripciones;
+// app.post('/index.hbs', async (req, res) => {
+//     const suscripciones = req.body.suscripciones;
     
-    if(suscripciones === false) {
-        let alerta = 'Ingrese un correo electronico';
-        res.render('index.hbs', {
-            alerta
-        })
+//     if(suscripciones === false) {
+//         let alerta = 'Ingrese un correo electronico';
+//         res.render('index.hbs', {
+//             alerta
+//         })
             
-    }  
-    else {    
-        let correo = {email: suscripciones}  
+//     }  
+//     else {    
+//         let correo = {email: suscripciones}  
 
-        const querySelect = 'SELECT * FROM suscripcion WHERE ?' 
-        connection.query(querySelect,correo, 
-            function(err, results, fields) {
+//         const querySelect = 'SELECT * FROM suscripcion WHERE ?' 
+//         connection.query(querySelect,correo, 
+//             function(err, results, fields) {
 
-            if(results.length == 0) {
+//             if(results.length == 0) {
         
-                let query = 'INSERT INTO suscripcion SET ?';
+//                 let query = 'INSERT INTO suscripcion SET ?';
     
-                connection.query(query,correo);
-                emailSuscripcion(suscripciones); 
-                res.render('nuevosub.hbs')
-            } else {
+//                 connection.query(query,correo);
+//                 emailSuscripcion(suscripciones); 
+//                 res.render('nuevosub.hbs')
+//             } else {
                 
-                let alerta = 'Ya se encuentra suscripto';
-                res.render('suscripto.hbs', {
-                    alerta
-                })
+//                 let alerta = 'Ya se encuentra suscripto';
+//                 res.render('suscripto.hbs', {
+//                     alerta
+//                 })
                 
-            }
-        });
-    }
-});
+//             }
+//         });
+//     }
+// });
 
 
 
     
 
 
-    async function emailSuscripcion(suscripciones) {
-        let transportador = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.USERMAIL,
-                pass: process.env.PASS,
-            }
-        });
+    // async function emailSuscripcion(suscripciones) {
+    //     let transportador = nodemailer.createTransport({
+    //         host: 'smtp.gmail.com',
+    //         port: 465,
+    //         secure: true,
+    //         auth: {
+    //             user: process.env.USERMAIL,
+    //             pass: process.env.PASS,
+    //         }
+    //     });
 
-        let confirmacionSuscripcion = await transportador.sendMail({
-            from: process.env.USERMAIL,
-            to: `${suscripciones}`,
-            subject: 'Hotel Miramar Suscripcion',
-            text: 'Proximamente recibira las mejores ofertas y promociones de nuestro Hotel. Gracias por susbscribirse',
-        })
-    }
+    //     let confirmacionSuscripcion = await transportador.sendMail({
+    //         from: process.env.USERMAIL,
+    //         to: `${suscripciones}`,
+    //         subject: 'Hotel Miramar Suscripcion',
+    //         text: 'Proximamente recibira las mejores ofertas y promociones de nuestro Hotel. Gracias por susbscribirse',
+    //     })
+    // }
     
 
 
@@ -108,35 +108,35 @@ app.get('/consultas.hbs', (req, res) => {
 })
 
 
-app.post ('/consultas.hbs', (req, res) => {
-    const {nombre, apellido, email, telefono, textarea} = req.body;
+// app.post ('/consultas.hbs', (req, res) => {
+//     const {nombre, apellido, email, telefono, textarea} = req.body;
     
-    if (!nombre || !apellido || !email || !telefono || !textarea) {
+//     if (!nombre || !apellido || !email || !telefono || !textarea) {
 
-        let datos = 'POR FAVOR RELLENE TODOS LOS CAMPOS!!';
-        res.render('consultas', {
-            datos
-        })
+//         let datos = 'POR FAVOR RELLENE TODOS LOS CAMPOS!!';
+//         res.render('consultas', {
+//             datos
+//         })
 
-    } else {
+//     } else {
 
-        let informacion = {
-            nombre: nombre,
-            apellido: apellido,
-            email: email,
-            telefono: +telefono,
-            textarea: textarea
-        }
+//         let informacion = {
+//             nombre: nombre,
+//             apellido: apellido,
+//             email: email,
+//             telefono: +telefono,
+//             textarea: textarea
+//         }
     
-        let sqlQuery = 'INSERT INTO consultas SET ?';
+//         let sqlQuery = 'INSERT INTO consultas SET ?';
     
-        connection.query(sqlQuery,informacion);
+//         connection.query(sqlQuery,informacion);
             
-        res.render('gracias')
-    }
+//         res.render('gracias')
+//     }
 
 
-})
+// })
 
 
 
